dafb8d8a6931e40ce025578e7faf8b8e603a644c
Christian Payne <christianpayne522@gmail.com>
2021-06-04 21:26:45 -0700

    Added router and form testing.

---
+++ src/pages/forms.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  export let name = "World";
+</script>
+
+<form name="Test Contact Form" data-netlify="true">
+  <p>
+    <label>Name <input type="text" name="name" /></label>
+  </p>
+  <p>
+    <label>Email <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
+
+<style>
+  h1 {
+    margin: 0;
+  }
+</style>
