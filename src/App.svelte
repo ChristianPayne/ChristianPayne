9d14b4fb0f15fc1e5cfdcd7b7e5b7b9112954570
Christian Payne <christianpayne522@gmail.com>
2021-06-04 20:24:41 -0700

    Added basic Under Construction page.

--- src/App.svelte
+++ src/App.svelte
@@ -1,65 +1,39 @@
-<script lang="ts">
-  import logo from './assets/svelte.png'
-  import Counter from './lib/Counter.svelte'
-</script>
+<div class="center">
+  <div>
+    <h1>🛠 Under Construction 🛠</h1>
+    <a href="https://www.github.com/ChristianPayne" target="_blank">Check me out over on <span>GitHub</span>!</a>
+    <h3>christianpayne.dev</h3>
+  </div>
+</div>
 
-<main>
-  <img src={logo} alt="Svelte Logo" />
-  <h1>Hello Typescript!</h1>
-
-  <Counter />
-
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
-</main>
 
 <style>
-  :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
-
-  main {
+  .center {
+    font-family: sans-serif;
+    margin: 0;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
     text-align: center;
-    padding: 1em;
-    margin: 0 auto;
-  }
-
-  img {
-    height: 16rem;
-    width: 16rem;
   }
 
   h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4rem;
-    font-weight: 100;
-    line-height: 1.1;
-    margin: 2rem auto;
-    max-width: 14rem;
+    font-size: 1.5em;
+    color: #f8f8f2;
+  }
+  h2, a {
+    color: #6272a4;
+  }
+  h3 {
+    color: #44475a;
   }
 
-  p {
-    max-width: 14rem;
-    margin: 1rem auto;
-    line-height: 1.35;
+  a {
+    text-decoration: none;
   }
 
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
-    }
-
-    p {
-      max-width: none;
-    }
+  a span {
+    color: #f1fa8c;
   }
 </style>
