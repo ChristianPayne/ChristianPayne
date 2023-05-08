804da8aa166404330a5c29cc049a4b68c943cd3f
Christian Payne <christianpayne522@gmail.com>
2023-05-07 23:28:35 -0700

    Well, new year, new website (even though it is May).

--- src/pages/home.svelte
+++ src/pages/home.svelte
@@ -16,8 +16,6 @@
    'Attention to detail'
  ]
 }
-<a href="tel:16572341562">contact(phone)</a>
-<a href="mailto:christianpayne522@gmail.com">contact(email)</a>
 `
 </script>
 
@@ -25,7 +23,7 @@
 <hr/>
 <Quote class="text-xl"/>
 <hr/>
-<div class="md:flex space-y-4 md:space-y-0">
+<div class="xl:flex space-y-4 md:space-y-2">
   <div class="grow space-y-2">
     <p>I am a developer who works with code to solve problems.</p>
     <p>I pride myself on attention to detail and creating strong foundations for others to build off of.</p>
@@ -33,12 +31,28 @@
   </div>
   <pre class="flex-none">
     {@html codeSnippet}
+    <Button type="link" linkRef="tel:16572341562">contact(christian.phone)</Button>
+    <Button type="link" linkRef="mailto:christianpayne522@gmail.com">contact(christian.email)</Button>
   </pre>
 </div>
 
-<Button type="default" onClick={() => console.log("JK, we have an onClick.")}></Button>
-<Button type="simple"></Button>
-<Button type="link" linkRef="#"></Button>
-<h2 class="text-2xl">Case Studies</h2>
+<!-- <Button type="default" onClick={() => console.log("JK, we have an onClick.")}></Button> -->
+<!-- <Button type="simple"></Button> -->
+<!-- <Button type="link" linkRef="#"></Button> -->
+<h2 class="text-2xl">Knowledge</h2>
+<div>
+  <p>Frontend Frameworks - Angular, React, Svelte</p>
+  <p>Css Frameworks - Tailwind, Material</p>
+  <p>Cloud Computing - AWS (Lambda, Glue), Google Cloud Platform (Firebase)</p>
+  <p>Databases - DynamoDB, Firestore, Mongo</p>
+  <p>Languages - TypeScript (JS), Python, GraphQL, SQL</p>
+</div>
+
+<h2 class="text-2xl">Interests</h2>
+<p>Other than coding, I spend my free time pursuing other things I find interesting. Sewing, long boarding, teaching, cooking and hobbyist computers make the list, to name a few.</p>
+
+<!-- <h2 class="text-2xl">Case Studies</h2>
+<h3 class="text-xl">The Event Community</h3>
+Nothing yet! -->
 <h2 class="text-2xl">Recent commits</h2>
 <GitHubRepos></GitHubRepos>
